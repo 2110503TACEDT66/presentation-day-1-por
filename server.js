df276a7ab7a1f1c4dcfdb2f04f398b204d42b0dd
Pookan Pattaradanai <datas.pookan@gmail.com>
2024-03-03 14:36:36 +0700

    updateServer.js

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const cookieParser=require('cookie-parser');
 //Imports a function
 const connectDB = require('./config/db');
 
+//Addtional
+const mongoSanitize=require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
 //Read environment variables
 dotenv.config({path:'./config/config.env'});
 
@@ -17,12 +25,25 @@ connectDB();
 const camps = require('./routes/camps');
 const auth = require('./routes/auth');
 const reservations = require('./routes/reservations');
+const limiter = rateLimit({
+    windowMs: 10*60*1000, //10 mins
+    max: 500
+});
+
 
 //Creates an instance of the Express application
 const app=express();
 
 //Adds middleware to parse incoming request bodies with JSON payloads
 app.use(express.json());
+
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
+
 //Adds the cookie-parser middleware to parse cookies attached to incoming requests
 app.use(cookieParser());
 
@@ -30,7 +51,7 @@ app.use(cookieParser());
 //Mount routers
 app.use('/api/v1/camps',camps);
 app.use('/api/v1/auth',auth);
-app.use('/api/v1/appointments',appointments);
+app.use('/api/v1/reservations',reservations);
 
 //Sets the port for the Express server. It either uses the value of the PORT environment variable or defaults to port 5100
 const PORT=process.env.PORT || 5100;
